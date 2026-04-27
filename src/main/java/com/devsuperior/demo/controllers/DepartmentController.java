201a618e144935db0e617b3254dedf2025433cf4
jeferson <jsmartins959@gmail.com>
2026-04-27 17:33:55 -0300

    findAll integration test code

---
+++ src/main/java/com/devsuperior/demo/controllers/DepartmentController.java
@@ -0,0 +1,29 @@
+package com.devsuperior.demo.controllers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.demo.dto.DepartmentDTO;
+import com.devsuperior.demo.entities.Department;
+import com.devsuperior.demo.repositories.DepartmentRepository;
+
+@RestController
+@RequestMapping(value = "/departments")
+public class DepartmentController {
+
+	@Autowired
+	private DepartmentRepository repository;
+	
+	@GetMapping
+	public List<DepartmentDTO> findAll(){
+		
+		List<Department> list = repository.findAll(Sort.by("name"));
+		return list.stream().map(x -> new DepartmentDTO(x)).collect(Collectors.toList());
+	}
+}
