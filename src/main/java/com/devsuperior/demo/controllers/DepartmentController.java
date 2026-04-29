9ac73e8ec29c1d8f7bb40c1dd99fceb8945e93ac
jeferson <jsmartins959@gmail.com>
2026-04-29 17:42:02 -0300

    fix: DepartmentService and DepartmentController

--- src/main/java/com/devsuperior/demo/controllers/DepartmentController.java
+++ src/main/java/com/devsuperior/demo/controllers/DepartmentController.java
@@ -1,29 +1,27 @@
 package com.devsuperior.demo.controllers;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.demo.dto.DepartmentDTO;
-import com.devsuperior.demo.entities.Department;
-import com.devsuperior.demo.repositories.DepartmentRepository;
+import com.devsuperior.demo.services.DepartmentService;
 
 @RestController
 @RequestMapping(value = "/departments")
 public class DepartmentController {
 
 	@Autowired
-	private DepartmentRepository repository;
+	private DepartmentService service;
 	
 	@GetMapping
-	public List<DepartmentDTO> findAll(){
+	public ResponseEntity<List<DepartmentDTO>> findAll(){
 		
-		List<Department> list = repository.findAll(Sort.by("name"));
-		return list.stream().map(x -> new DepartmentDTO(x)).collect(Collectors.toList());
+		List<DepartmentDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 }
